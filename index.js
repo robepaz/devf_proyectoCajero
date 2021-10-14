187e40a7b2c6a9e97578f05b408e9666d47cfb29
Roberto Paz <fsjedi@outlook.com>
2021-10-14 17:07:27 -0400

    agregamos funciones finales

--- index.js
+++ index.js
@@ -9,55 +9,44 @@ var cuentas = [
 //Variables de validación de credenciales
 var validacionNombre = false
 var validacionPassword = false
+var usuarioDatos
 
 //Usuario introduce credenciales para acceder al sistema
-const usuarioNombre = prompt('Introduce tu nombre de usuario')
+//const usuarioNombre = prompt('Introduce tu nombre de usuario')
 
+//Esta funcion es llamada cuando se ha validado al usuario
 function revisaPassword() {
-  console.log('entro a funcion revisaPassword')
   while (validacionPassword === false) {
     let usuarioPassword = prompt('Introduce tu password')
 
-    if (busca.password === usuarioPassword) {
-      console.log(`Hola ${busca.nombre} bienvenid@, puedes hacer transacciones`)
+    if (usuarioDatos.password === usuarioPassword) {
+      console.log(
+        `Hola ${usuarioDatos.nombre} bienvenid@, puedes hacer transacciones`
+      )
       validacionPassword = true
     } else
       console.log('Lo siento, tu password es incorrecto.  Intenta nuevamente.') // no existe
   }
 }
 
-//Ejecuta esta funcion para validar si el nombre de usuario existe
-if (validacionNombre === false) {
+//Valida si el nombre de usuario existe y permanece hasta que el usuario introduzca un nombre válido
+while (validacionNombre === false) {
+  const usuarioNombre = prompt('Introduce tu nombre de usuario')
   const busca = cuentas.find(({ nombre }) => nombre === usuarioNombre)
   if (busca) {
-    console.log('entro a verdadero')
     validacionNombre = true
+    usuarioDatos = cuentas.find(({ nombre }) => nombre === usuarioNombre)
     revisaPassword()
   } else {
-    console.log('no existe tal usuario')
+    console.log('no existe tal usuario, por favor ingresa nuevamente')
   }
 }
 
 console.log('hasta aqui todo bien')
 
-/*   Backup, esta funcionando, hacemos esto para cambiar a funcion
-//Valida si usuario existe y su password es correcto
-const busca = cuentas.find(({ nombre }) => nombre === usuarioNombre)
-if (busca) {
-  //console.log(busca.password) // devuelve datos de usuario
-  validacionNombre = true
-  const usuarioPassword = prompt('Introduce tu password')
+let opcion = prompt('Introduce 1. Para )
 
-  if (busca.password === usuarioPassword) {
-    
-    console.log(`Hola ${busca.nombre}, puedes hacer transacciones`)
-    
-  }
-} else {
 
-  console.log('Lo siento, no existe un usuario con tu nombre.  Intenta nuevamente.') // no existe
-}
-*/
 
 /*
 Al seleccionar una cuenta, debes ingresar el password asociado a la cuenta. Si el password
